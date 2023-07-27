04b9b8e63a0236a9baa4635b7f5bff6aa57e8736
PAULO VINICIUS NASCIMENTO SANTOS DE CARVALHO <paulo.carvalho@trt8.jus.br>
2023-07-27 02:06:42 -0300

    Cuidando de uma possivel internacionalização. Também foram alterados alguns rótulos da tela de login e a imagem do Firebase foi substituída por a imagem do app.

--- lib/ui/screens/tela_login.dart
+++ lib/ui/screens/tela_login.dart
@@ -1,21 +1,16 @@
 import 'dart:convert';
 
 import 'package:duraes/config/config.dart';
+import 'package:duraes/config/rotulos.dart';
 import 'package:duraes/main.dart';
 import 'package:duraes/ui/components/decorations.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
-import 'package:firebase_ui_localizations/firebase_ui_localizations.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-
-class LabelOverrides extends DefaultLocalizations {
-  const LabelOverrides();
-
-  @override
-  String get emailInputLabel => 'Enter your email';
-}
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:firebase_ui_localizations/firebase_ui_localizations.dart';
 
 class FirebaseAuthUIExample extends StatelessWidget {
   const FirebaseAuthUIExample({super.key});
@@ -113,15 +108,15 @@ class FirebaseAuthUIExample extends StatelessWidget {
             styles: const {
               EmailFormStyle(signInButtonVariant: ButtonVariant.filled),
             },
-            headerBuilder: headerImage('assets/images/flutterfire_logo.png'),
-            sideBuilder: sideImage('assets/images/flutterfire_logo.png'),
+            headerBuilder: headerImage('assets/images/image_launcher.png'),
+            sideBuilder: sideImage('assets/images/image_launcher.png'),
             subtitleBuilder: (context, action) {
               return Padding(
                 padding: const EdgeInsets.only(bottom: 8),
                 child: Text(
                   action == AuthAction.signIn
-                      ? 'Welcome to Firebase UI! Please sign in to continue.'
-                      : 'Welcome to Firebase UI! Please create an account to continue',
+                      ? 'Bem vindo ao Acelerador de Ecommerce!'
+                      : 'Bem vindo ao Acelerador de Ecommerce! Por favor, crie um conta para continuar.',
                 ),
               );
             },
@@ -131,8 +126,8 @@ class FirebaseAuthUIExample extends StatelessWidget {
                   padding: const EdgeInsets.only(top: 16),
                   child: Text(
                     action == AuthAction.signIn
-                        ? 'By signing in, you agree to our terms and conditions.'
-                        : 'By registering, you agree to our terms and conditions.',
+                        ? ''
+                        : '',
                     style: const TextStyle(color: Colors.grey),
                   ),
                 ),
@@ -301,15 +296,15 @@ class FirebaseAuthUIExample extends StatelessWidget {
             styles: const {
               EmailFormStyle(signInButtonVariant: ButtonVariant.filled),
             },
-            headerBuilder: headerImage('assets/images/flutterfire_logo.png'),
-            sideBuilder: sideImage('assets/images/flutterfire_logo.png'),
+            headerBuilder: headerImage('assets/images/image_launcher.png'),
+            sideBuilder: sideImage('assets/images/image_launcher.png'),
             subtitleBuilder: (context, action) {
               return Padding(
                 padding: const EdgeInsets.only(bottom: 8),
                 child: Text(
                   action == AuthAction.signIn
-                      ? 'Welcome to Firebase UI! Please sign in to continue.'
-                      : 'Welcome to Firebase UI! Please create an account to continue',
+                      ? 'Bem vindo ao Acelerador de Ecommerce!'
+                      : 'Bem vindo ao Acelerador de Ecommerce! Por favor, crie um conta para continuar.',
                 ),
               );
             },
@@ -331,12 +326,17 @@ class FirebaseAuthUIExample extends StatelessWidget {
       },
       title: 'Login',
       debugShowCheckedModeBanner: false,
-      locale: const Locale('en'),
+      locale: const Locale('pt'),
       localizationsDelegates: [
         FirebaseUILocalizations.withDefaultOverrides(const LabelOverrides()),
-        //GlobalMaterialLocalizations.delegate,
-        //GlobalWidgetsLocalizations.delegate,
         FirebaseUILocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        Locale('en'),
+        Locale('pt'),
       ],
     );
   }
